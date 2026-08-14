4be23b5caa80d761bfe96de4f5eac1fe39abedac
Enzo Mazur Traini <enzotraini8@gmail.com>
2026-08-14 10:51:18 -0300

    Update EMT_SYSTEM_URL to the new domain for the EMT system

--- src/utils/links.js
+++ src/utils/links.js
@@ -1,2 +1,2 @@
 export const STEELFLOW_URL = 'https://saa-s-sistema-para-cortes.vercel.app'
-export const EMT_SYSTEM_URL = 'https://taruma.emtsistemas.com.br/auth/sign-in'
+export const EMT_SYSTEM_URL = 'https://emtsistemas.com.br'
